b2c55c1b22a1e313642b770f8d69576a789d5a25
kanggicheon <snoopy1516@naver.com>
2023-06-24 20:37:43 +0900

    Todos delete object
    app.js로 todos배열에서 id로 항목 지우기,
    TodoListItem에서 삭제 함수 호출

---
+++ TodoListItem_deleteFunction.js
@@ -0,0 +1,27 @@
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames'
+import './TodoListItem.scss';
+
+const TodoListItem = ({todo,onRemove}) => {
+
+  const {id,text, checked} = todo;
+
+  return (
+    <div className="TodoListItem">
+      <div className={cn("checkbox",{checked})}>
+        {checked ?<MdCheckBox /> : <MdCheckBoxOutlineBlank/>}
+        <div className="text">{text}</div>
+      </div>
+
+      <div className="remove" onClick={()=> onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
---
+++ TodoList_deleteFunction.js
@@ -0,0 +1,16 @@
+import TodoListItem from "./TodoListItem";
+import './TodoList.scss';
+
+const TodoList = ({todos,onRemove}) => {
+    return (
+        <div className="TodoList">
+
+            {todos.map(todo => (
+                <TodoListItem todo = {todo} key={todo.id} onRemove={onRemove} />
+            ))}
+
+        </div>
+    );
+};
+
+export default TodoList;
